730319e9e7fdab00eaf6c9143ac22e948ec6f5be
Natalia Meneses <nmeneses8@gmail.com>
2020-09-30 23:09:26 -0500

    Docs agregado

---
+++ docs/precache-manifest.8482cc5f7c0e60123a93278f1efa6bea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76ea143f84a0b886992a4103db45445f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "987553f5853fa69c03c6",
+    "url": "/static/css/main.2c74f117.chunk.css"
+  },
+  {
+    "revision": "888035f00e6def5a12d4",
+    "url": "/static/js/2.9f69416b.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/static/js/2.9f69416b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "987553f5853fa69c03c6",
+    "url": "/static/js/main.15c4ecab.chunk.js"
+  },
+  {
+    "revision": "0eafbac4c93c5b09ed1c",
+    "url": "/static/js/runtime-main.e4185f56.js"
+  }
+]);
